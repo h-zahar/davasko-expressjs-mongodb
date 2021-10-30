c669c4c3af8d5fb757b6834c9cacfbefe266f4b9
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-10-30 22:30:46 +0600

    GET and POST Request Included

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ObjectId } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gmeoo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+    res.send("Server Running Happily");
+});
+
+
+// func mongo
+const run = async() => {
+    try {
+        await client.connect();
+        const database = client.db(process.env.DB_NAME);
+        const offerings = database.collection(process.env.DB_COLLECTION_OFFERING);
+        const orders = database.collection(process.env.DB_COLLECTION_ORDER);
+
+        app.get('/offerings', async (req, res) => {
+            const cursor = offerings.find({});
+        
+            const dataReturn = async() => {
+                return cursor.toArray();
+            };
+            
+            const results = await dataReturn();
+            res.send(results);
+        });
+
+        app.get('/offerings/:id', async (req, res) => {
+            const {id} = req.params;
+            const query = { _id: ObjectId(id) }
+
+            const result = await offerings.findOne(query);
+            res.send(result);
+        });
+
+        app.get('/orders', async (req, res) => {
+            const result = await orders.findOne({});
+
+            if (result) {
+                res.send(result);
+            }
+            else {
+                res.send('Waiting for Data');
+            }
+        });
+
+        app.post('/orders', async (req, res) => {
+            const doc = req.body;
+            const result = await orders.insertOne(doc);
+        });
+
+    } finally {
+        // await client.close();
+    }
+};
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Listening at port ${port}`);
+});

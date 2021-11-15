d5c0c6d77203c13b93fef5aa1f62a26e3df6307c
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-16 01:04:02 +0600

    Rearranged

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
+const app = express();
 
 const port = process.env.PORT || 5000;
 
@@ -138,14 +138,8 @@ const run = async() => {
     } finally {
         // await client.close();
     }
-}
+};
 run().catch(console.dir);
-
-app.get('/', (req, res) => {
-    res.send("Server Running Happily...");
-});
-
-//
 
 app.listen(port, () => {
     console.log(`Listening at port ${port}`);

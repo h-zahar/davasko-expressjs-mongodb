a9955029c094d77e608c7ddfed7992f179b69239
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-16 00:39:35 +0600

    Rearranged

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.get('/', (req, res) => {
+    res.send("Server Running Happily...");
+});
+
 
 const run = async() => {
     try {
@@ -138,9 +142,6 @@ const run = async() => {
 };
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send("Server Running Happily...");
-});
 
 app.listen(port, () => {
     console.log(`Listening at port ${port}`);
